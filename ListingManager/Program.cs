40b0b9c42e30164af3fe2504cb36b31eca9fdc5a
Benjamin Michaelis <benjamin@michaelis.net>
2023-10-12 20:43:04 +0100

    feat: Switch to use System.CommandLine (#78)
    * Switch to use system.commandline
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

--- ListingManager/Program.cs
+++ ListingManager/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.CommandLine;
+using System.IO;
 using System.Linq;
 
 namespace EssentialCSharp.ListingManager;
@@ -12,51 +14,75 @@ public class Program
   | | | '_ \| __/ _ \ | | |  | |/ _ \/ __| __|
  _| |_| | | | ||  __/ | | |  | |  __/ (__| |_ 
 |_____|_| |_|\__\___|_|_|_|  |_|\___|\___|\__|";
-
-    public static void Main(string path = "", ListingModes mode = ListingModes.ListingUpdating,
-        bool verbose = false,
-        bool preview = false,
-        bool byFolder = false,
-        bool chapterOnly = false)
+    private static int Main(string[] args)
     {
-        while (path.EndsWith("\\"))
-        {
-            path = path.Remove(path.Length - 1, 1);
-        }
-        Console.WriteLine(IntelliTect);
+        var directoryIn = new Option<DirectoryInfo?>(
+            name: "--path",
+            description: "The directory of the chapter to update listings on.")
+        { IsRequired = true };
 
-        if (preview)
-        {
-            Console.WriteLine("Preview mode. Actions will not be taken");
-        }
+        // With proper logging implemented, this option will hopefully be removed
+        var verboseOption = new Option<bool>(
+            name: "--verbose",
+            description: "Displays more detailed messages in the log");
 
-        if (string.IsNullOrWhiteSpace(path))
-        {
-            ConsoleColor foregroundColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            path = Environment.CurrentDirectory;
-            Console.WriteLine($"Path not specified. Using working directory: {path}");
-            Console.ForegroundColor = foregroundColor;
-        }
+        var previewOption = new Option<bool>(
+            name: "--preview",
+            description: "Writes all logs to console as if changes will be made without actually making changes.");
 
-        switch (mode)
-        {
-            case ListingModes.ListingUpdating:
-                Console.WriteLine($"Updating listing namespaces of: {path}");
-                ListingManager.UpdateChapterListingNumbers(path, verbose, preview, byFolder, chapterOnly, false);
-                break;
-            case ListingModes.ScanForMismatchedListings:
-                var extraListings = ListingManager.GetAllExtraListings(path).OrderBy(x => x);
+        // TODO: Add better descriptions when their functionality becomes clearer
+        var byFolderOption = new Option<bool>(
+            name: "--byfolder",
+            description: "");
 
-                Console.WriteLine("---Extra Listings---");
-                foreach (string extraListing in extraListings)
-                {
-                    Console.WriteLine(extraListing);
-                }
-                break;
-            default:
-                Console.WriteLine($"Mode ({mode}) does not exist. Exiting");
-                break;
-        }
+        // TODO: Add better descriptions when their functionality becomes clearer
+        var chapterOnlyOption = new Option<bool>(
+            name: "--chapteronly",
+            description: "");
+
+        var listingUpdating = new Command("update", "Updates namespaces and filenames for all lisitngs and accompanying tests within a chapter")
+        {
+            directoryIn,
+            verboseOption,
+            previewOption,
+            byFolderOption,
+            chapterOnlyOption
+        };
+
+        // Give better description when intent and functionality becomes more flushed out
+        var scanForMismatchedListings = new Command("scan", "Scans for mismatched listings")
+        {
+            directoryIn
+        };
+
+
+        var rootCommand = new RootCommand()
+        {
+            listingUpdating,
+            scanForMismatchedListings
+        };
+
+        listingUpdating.SetHandler((directoryIn, verbose, preview, byFolder, chapterOnly) =>
+        {
+            Console.WriteLine(IntelliTect);
+            Console.WriteLine($"Updating listing namespaces of: {directoryIn}");
+            // TODO: Add option for last parameter
+            // TODO: Change parameter to take a DirectoryInfo instead of a string: https://github.com/IntelliTect/ListingManager/issues/26
+            ListingManager.UpdateChapterListingNumbers(directoryIn!.FullName, verbose, preview, byFolder, chapterOnly, false);
+        }, directoryIn, verboseOption, previewOption, byFolderOption, chapterOnlyOption);
+
+        scanForMismatchedListings.SetHandler((directoryIn) =>
+        {
+            Console.WriteLine(IntelliTect);
+            var extraListings = ListingManager.GetAllExtraListings(directoryIn!.FullName).OrderBy(x => x);
+
+            Console.WriteLine("---Extra Listings---");
+            foreach (string extraListing in extraListings)
+            {
+                Console.WriteLine(extraListing);
+            }
+        }, directoryIn);
+
+        return rootCommand.Invoke(args);
     }
 }
