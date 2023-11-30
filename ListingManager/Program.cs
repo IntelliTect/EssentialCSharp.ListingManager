ca3b3e18bf03b0b00ffb6f6525eafc3a14c2acd1
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-30 09:20:29 -0800

    feat: Update to System.CommandLine (#110)

--- ListingManager/Program.cs
+++ ListingManager/Program.cs
@@ -2,47 +2,54 @@ using System.CommandLine;
 
 namespace EssentialCSharp.ListingManager;
 
-public class Program
+public sealed class Program
 {
-    private const string IntelliTect =
-@" _____       _       _ _    _______        _   
-  |_   _|     | |     | | ( )|__   __|      | |  
-    | |  _ __ | |_ ___| | |_    | | ___  ___| |_ 
-    | | | '_ \| __/ _ \ | | |   | |/ _ \/ __| __|
-   _| |_| | | | ||  __/ | | |   | |  __/ (__| |_ 
-  |_____|_| |_|\__\___|_|_|_|   |_|\___|\___|\__|";
-    private static int Main(string[] args)
+    private static Task<int> Main(string[] args)
     {
-        var directoryIn = new Option<DirectoryInfo>(
-            name: "--path",
-            description: "The directory of the chapter to update listings on.")
-        { IsRequired = true };
+        CliConfiguration configuration = GetConfiguration();
+        return configuration.InvokeAsync(args);
+    }
+
+    public static CliConfiguration GetConfiguration()
+    {
+        // Use the ExistingOnly method to only parse the arguments that are defined in the configuration
+
+        CliArgument<DirectoryInfo> directoryInArgument = new("directoryIn")
+        {
+            Description = "The directory of the chapter to update listings on.",
+        };
+        directoryInArgument.AcceptExistingOnly();
 
         // With proper logging implemented, this option will hopefully be removed
-        var verboseOption = new Option<bool>(
-            name: "--verbose",
-            description: "Displays more detailed messages in the log");
+        CliOption<bool> verboseOption = new("--verbose")
+        {
+            Description = "Displays more detailed messages in the log.",
+        };
 
-        var previewOption = new Option<bool>(
-            name: "--preview",
-            description: "Writes all logs to console as if changes will be made without actually making changes.");
+        CliOption<bool> previewOption = new("--preview")
+        {
+            Description = "Displays the changes that will be made without actually making them.",
+        };
 
-        var allChaptersOption = new Option<bool>(
-           name: "--allChapters",
-           description: "The passed in path is the parent directory to many chapter directories rather than a single chapter directory.");
+        CliOption<bool> allChaptersOption = new("--all-chapters")
+        {
+            Description = "The passed in path is the parent directory to many chapter directories rather than a single chapter directory.",
+        };
 
         // TODO: Add better descriptions when their functionality becomes clearer
-        var byFolderOption = new Option<bool>(
-            name: "--byfolder",
-            description: "");
-
-        var singleDirOption = new Option<bool>(
-            name: "--singleDir",
-            description: "All listings are in a single directory and not separated into chapter and chapter test directories");
-
-        var listingUpdating = new Command("update", "Updates namespaces and filenames for all listings and accompanying tests within a chapter")
+        CliOption<bool> byFolderOption = new("--by-folder")
         {
-            directoryIn,
+            Description = "Updates namespaces and filenames for all listings and accompanying tests within a folder.",
+        };
+
+        CliOption<bool> singleDirOption = new("--single-dir")
+        {
+            Description = "All listings are in a single directory and not separated into chapter and chapter test directories.",
+        };
+
+        CliCommand listingUpdating = new("update", "Updates namespaces and filenames for all listings and accompanying tests within a chapter")
+        {
+            directoryInArgument,
             verboseOption,
             previewOption,
             byFolderOption,
@@ -50,36 +57,15 @@ public class Program
             allChaptersOption
         };
 
-        // Give better description when intent and functionality becomes more flushed out
-        var scan = new Command("scan", "Scans for various things")
+        listingUpdating.SetAction((ParseResult parseResult) =>
         {
-            directoryIn
-        };
+            DirectoryInfo directoryIn = parseResult.CommandResult.GetValue(directoryInArgument)!;
+            bool verbose = parseResult.CommandResult.GetValue(verboseOption);
+            bool preview = parseResult.CommandResult.GetValue(previewOption);
+            bool byFolder = parseResult.CommandResult.GetValue(byFolderOption);
+            bool singleDir = parseResult.CommandResult.GetValue(singleDirOption);
+            bool allChapters = parseResult.CommandResult.GetValue(allChaptersOption);
 
-        var scanForMismatchedListings = new Command("listings", "Scans for mismatched listings")
-        {
-            directoryIn
-        };
-
-        var scanForMissingTests = new Command("tests", "Scans for missing tests")
-        {
-            directoryIn,
-            allChaptersOption,
-            singleDirOption
-        };
-
-        scan.AddCommand(scanForMismatchedListings);
-        scan.AddCommand(scanForMissingTests);
-
-        var rootCommand = new RootCommand()
-        {
-            listingUpdating,
-            scan
-        };
-
-        listingUpdating.SetHandler((directoryIn, verbose, preview, byFolder, singleDir, allChapters) =>
-        {
-            Console.WriteLine(IntelliTect);
             Console.WriteLine($"Updating listings within: {directoryIn}");
             ListingManager listingManager = new(directoryIn);
             if (allChapters)
@@ -90,23 +76,40 @@ public class Program
             {
                 listingManager.UpdateChapterListingNumbers(directoryIn, verbose, preview, byFolder, singleDir);
             }
-        }, directoryIn, verboseOption, previewOption, byFolderOption, singleDirOption, allChaptersOption);
+        });
 
-        scanForMismatchedListings.SetHandler((directoryIn) =>
+        CliCommand scan = new("scan", "Scans for various things");
+
+        CliCommand listings = new("listings", "Scans for mismatched listings")
         {
-            Console.WriteLine(IntelliTect);
-            var extraListings = ListingManager.GetAllExtraListings(directoryIn!.FullName).OrderBy(x => x);
+            directoryInArgument
+        };
+
+        listings.SetAction((ParseResult parseResult) =>
+        {
+            DirectoryInfo directoryIn = parseResult.CommandResult.GetValue(directoryInArgument)!;
+            var extraListings = ListingManager.GetAllExtraListings(directoryIn.FullName).OrderBy(x => x);
 
             Console.WriteLine("---Extra Listings---");
             foreach (string extraListing in extraListings)
             {
                 Console.WriteLine(extraListing);
             }
-        }, directoryIn);
+        });
+        scan.Subcommands.Add(listings);
 
-        scanForMissingTests.SetHandler((directoryIn, allChapters, singleDir) =>
+        CliCommand tests = new("tests", "Scans for mismatched tests")
         {
-            Console.WriteLine(IntelliTect);
+            directoryInArgument,
+            allChaptersOption,
+            singleDirOption
+        };
+
+        tests.SetAction((ParseResult parseResult) =>
+        {
+            DirectoryInfo directoryIn = parseResult.CommandResult.GetValue(directoryInArgument)!;
+            bool allChapters = parseResult.CommandResult.GetValue(allChaptersOption);
+            bool singleDir = parseResult.CommandResult.GetValue(singleDirOption);
 
             Console.WriteLine("---Missing Tests---");
             if (allChapters)
@@ -117,8 +120,16 @@ public class Program
             {
                 ScanManager.ScanForMissingTests(directoryIn, singleDir);
             }
-        }, directoryIn, allChaptersOption, singleDirOption);
+        });
 
-        return rootCommand.Invoke(args);
+        scan.Subcommands.Add(tests);
+
+        CliRootCommand rootCommand = new("The EssentialCSharp.ListingManager helps to organize and manage the EssentialCSharp source code")
+        {
+            listingUpdating,
+            scan
+        };
+
+        return new CliConfiguration(rootCommand);
     }
 }
